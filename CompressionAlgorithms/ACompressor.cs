6369f0148302a811304019590bcdbc29182a90e4
Naor Tedgi <naor2205@gmail.com>
2017-05-05 14:44:38 +0300

    Add files via upload

---
+++ CompressionAlgorithms/ACompressor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectSrc.Model.CompressionAlgorithms
+{
+     public abstract class ACompressor : ICompressor
+    {
+        public abstract byte[] Compress(byte[] data);
+
+        public abstract byte[] Decompress(byte[] compressedData);
+    }
+}
---
+++ CompressionAlgorithms/ICompressor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectSrc.Model.CompressionAlgorithms
+{
+    public interface ICompressor
+    {
+       
+        // interface of all Compressor Algorithm
+      
+    
+        /// <summary>
+        /// Compress Mathod
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns>Array of byte</returns>
+   
+        byte[] Compress(byte[] data);
+        /// <summary>
+        /// DeCompress Mathod
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns>Array of byte</returns>
+        byte[] Decompress(byte[] compressedData);
+    }
+}
